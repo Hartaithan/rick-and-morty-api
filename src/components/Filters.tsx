e79b78413a85ffc5e4316dc592b90c59857382f8
hartaithan <hartaithan@gmail.com>
2021-12-09 18:32:59 +0900

    feat: handle Enter key for submit form

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -5,28 +5,34 @@ import { Col, Row, Form, Button } from "react-bootstrap";
 function Filters(props: any) {
   const { handleSubmit, inputs, setInputs } = props;
 
+  const handleKeyDown = (event: any) => {
+    if (event.key === "Enter") {
+      handleSubmit();
+    }
+  };
+
   return (
     <div className="filters">
-      <Row>
-        <Col xs={12} md={4}>
+      <Form as={Row} onKeyDown={handleKeyDown}>
+        <Form.Group as={Col} xs={12} md={4}>
           <Form.Control
             placeholder="Enter name..."
             onChange={(e) => setInputs({ ...inputs, name: e.target.value })}
           />
-        </Col>
-        <Col xs={12} md={4}>
+        </Form.Group>
+        <Form.Group as={Col} xs={12} md={4}>
           <Form.Control
             placeholder="Enter type..."
             onChange={(e) => setInputs({ ...inputs, type: e.target.value })}
           />
-        </Col>
-        <Col xs={12} md={4} className="mb-2">
+        </Form.Group>
+        <Form.Group as={Col} xs={12} md={4} className="mb-2">
           <Form.Control
             placeholder="Enter species..."
             onChange={(e) => setInputs({ ...inputs, species: e.target.value })}
           />
-        </Col>
-        <Col xs={12} md={4}>
+        </Form.Group>
+        <Form.Group as={Col} xs={12} md={4}>
           <Form.Select
             aria-label="Status select"
             onChange={(e) => setInputs({ ...inputs, status: e.target.value })}
@@ -36,8 +42,8 @@ function Filters(props: any) {
             <option value="dead">Dead</option>
             <option value="unknown">Unknown</option>
           </Form.Select>
-        </Col>
-        <Col xs={12} md={4}>
+        </Form.Group>
+        <Form.Group as={Col} xs={12} md={4}>
           <Form.Select
             aria-label="Gender select"
             onChange={(e) => setInputs({ ...inputs, gender: e.target.value })}
@@ -48,13 +54,13 @@ function Filters(props: any) {
             <option value="genderless">Genderless</option>
             <option value="unknown">Unknown</option>
           </Form.Select>
-        </Col>
-        <Col xs={12} md={4}>
+        </Form.Group>
+        <Form.Group as={Col} xs={12} md={4}>
           <Button className="w-100 h-100" onClick={() => handleSubmit()}>
             Search
           </Button>
-        </Col>
-      </Row>
+        </Form.Group>
+      </Form>
     </div>
   );
 }
