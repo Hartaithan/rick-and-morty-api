3658a5a8a6e533b8d753f6221a62f36e157c6c92
hartaithan <hartaithan@gmail.com>
2022-04-29 16:42:33 +0900

    fix: popover min sizes

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Spinner } from "react-bootstrap";
+import { ILoaderProps } from "../../models/LoaderModel";
 
-const Loader: React.FC = (props) => {
+const Loader: React.FC<ILoaderProps> = (props) => {
+  const { className = "" } = props;
   return (
-    <div className="loader" {...props}>
+    <div className={`loader ${className}`}>
       <Spinner animation="border" role="status">
         <span className="visually-hidden">Loading...</span>
       </Spinner>
